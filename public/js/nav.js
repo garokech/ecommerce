c3fddda54cfb92bb33b56cb464343e60ef89144e
Garo Kechkarian <garo.kechkarian@yahoo.com>
2023-02-12 14:30:58 -0800

    Creating Nav Bar

---
+++ public/js/nav.js
@@ -0,0 +1,26 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+    <div class="nav">
+        <img src="" class="brand-logo" alt="Logo">
+        <div class="nav-items">
+            <div class="search">
+                <input type="text" class="search-box" placeholder="Search here">
+                <button class="search-btn">Search</button>
+            </div>
+            <a href="#"><img src="img/user.png"alt="User symbol"></a>
+            <a href="#"><img src="img/cart.png"alt="Cart symbol"></a>
+        </div>
+    </div>
+    <ul class="links-container">
+        <li class="link-item"><a href="#" class="link">Home</a></li>
+        <li class="link-item"><a href="#" class="link">Women's</a></li>
+        <li class="link-item"><a href="#" class="link">Men's</a></li>
+        <li class="link-item"><a href="#" class="link">Kid's</a></li>
+        <li class="link-item"><a href="#" class="link">Accessories</a></li>
+    </ul>
+`;
+}
+
+createNav();
